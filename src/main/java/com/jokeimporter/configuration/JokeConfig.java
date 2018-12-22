c38a35a8ac79fd5d48e51da714404cea3864555f
Keith Collier <34369272+tockstaunt@users.noreply.github.com>
2018-12-22 01:35:45 -0600

    added xml based config

--- src/main/java/com/jokeimporter/configuration/JokeConfig.java
+++ src/main/java/com/jokeimporter/configuration/JokeConfig.java
@@ -4,11 +4,11 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class JokeConfig {
 
 
-    @Bean
+   // @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
